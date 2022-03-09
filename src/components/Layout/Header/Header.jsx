777195b5d880a69b2c06caba14107fab0c74d57a
Gohan-117holguin <85535657+Gohan-117holguin@users.noreply.github.com>
2022-03-09 16:25:06 -0500

    make a better

---
+++ src/components/Layout/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <>
+            <header>
+                <h1>API Rick and Morty</h1>
+            </header>
+        </>
+    )
+}
